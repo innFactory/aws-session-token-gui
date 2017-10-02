f80b4e458a8914ca42b5fe6cebd09d24a2120b1a
Tobias Jonas <t.jonas@innFactory.de>
2017-10-02 16:09:32 +0200

    feat: add switch for access and id token

--- app/actions/user.js
+++ app/actions/user.js
@@ -47,7 +47,8 @@ function _cognitoLogin(user, dispatch, getState) {
     cognitoUser.authenticateUser(authenticationDetails, {
         onSuccess: (result) => {
             // login was successful: store aws idToken to state
-            dispatch({type: "SET_TOKEN", token: result.getIdToken().getJwtToken()});
+            dispatch({type: "SET_ID_TOKEN", token: result.getIdToken().getJwtToken()});
+            dispatch({type: "SET_ACCESS_TOKEN", token: result.getAccessToken().getJwtToken()});
         },
         onFailure: (err) => {
              // login failed: set error as token to display it
